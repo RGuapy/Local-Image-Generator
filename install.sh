0bb900b28162b15b7ce7bcb6f69ec6c9cc3b1c48
Renan <renanguapy@gmail.com>
2026-05-07 15:58:49 -0300

    try 1.2

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+# FFmpeg dev headers are required to build PyAV (faster-whisper dependency)
+apt-get install -y ffmpeg libavformat-dev libavcodec-dev libavdevice-dev \
+    libavutil-dev libavfilter-dev libswscale-dev libswresample-dev
+
 pip install -r requirements.txt
